c897805ef638617852af05b11728b55af8c8faf2
Roomsrio <roomsrio7782@gmail.com>
2022-07-06 22:44:31 +0200

    first page

---
+++ index.js
@@ -0,0 +1,8 @@
+anime({
+    targets: "#svgAnim path",
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: "easeInOutExpo",
+    duration: 5000,
+    direction: "alternate",
+    loop: true,
+  });
