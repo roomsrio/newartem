271d2a4e6bb9753e53c034bb12fb5a7e77bb5f9c
Roomsrio <roomsrio7782@gmail.com>
2022-07-07 22:26:18 +0200

    final first page

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 anime({
-    targets: "#svgAnim path",
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: "easeInOutExpo",
-    duration: 5000,
-    direction: "alternate",
-    loop: true,
-  });
+  targets: "#svgAnim path",
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: "easeInOutExpo",
+  duration: 5000,
+  direction: "alternate",
+  loop: true,
+});
+
+const accueil = document.getElementById("accueil");
+const name = document.getElementById("name");
+
+accueil.addEventListener("mousemove", (e) => {
+  name.style.position = "absolute";
+  if (
+    (e.pageX <= window.innerWidth * 0.75) &
+    (e.pageX >= window.innerWidth * 0.25)
+  ) {
+    name.style.left = `calc(${e.pageX}px - 25vw)`;
+  } else if (e.pageX >= window.innerWidth * 0.75) {
+    name.style.right = 0;
+  } else {
+    name.style.left = 0;
+  }
+});
