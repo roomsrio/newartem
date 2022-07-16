e06619228c8a849f9de368b649781dc4846122ca
Roomsrio <roomsrio7782@gmail.com>
2022-07-16 23:15:29 +0200

    overlay presentation

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // animation de nom Artem dans accueil
 anime({
   targets: "#svgAnim path",
@@ -24,4 +23,42 @@ accueil.addEventListener("mousemove", (e) => {
   }
 });
 
-const navbar = document.querySelector(".navLinks");
+// coller la navBar au scroll
+
+const header = document.getElementById("header");
+const navbar = document.getElementById("navbar");
+let positionNavBar = navbar.offsetTop;
+
+function sticky() {
+  let posCurseur = this.pageYOffset;
+  if (positionNavBar - posCurseur < 15) {
+    header.classList.add("stickyHeader");
+    navbar.classList.add("stickyNav");
+    logo.style.display = "none";
+  } else {
+    header.classList.remove("stickyHeader");
+    navbar.classList.remove("stickyNav");
+    logo.style.display = "flex";
+  }
+}
+
+window.addEventListener("scroll", sticky);
+
+// apparition de l'overlay avec les bouton savoir
+
+const modalContainer = document.querySelector(".modal-container");
+const modalTriggers = document.querySelectorAll(".modal-trigger");
+
+modalTriggers.forEach((trigger) =>
+  trigger.addEventListener("click", toggleModal)
+);
+
+function toggleModal() {
+  modalContainer.classList.toggle("active");
+}
+
+const years = document.querySelectorAll(".years");
+const circleContainer = document.querySelectorAll(".circle-container");
+const yearsText = document.querySelectorAll(".yearsText");
+
+console.log(circleContainer.children[3].tagName);
